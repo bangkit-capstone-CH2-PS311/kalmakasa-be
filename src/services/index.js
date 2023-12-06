c0612ba9f2c13e27518b4d9f538786c77961cf3a
FnHafiz <farhanhafiz55@gmail.com>
2023-12-06 16:26:02 +0700

    fix : index export for journal validation service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
-module.exports.authService = require('./auth.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.reservationService = require('./reservation.service');
-module.exports.consultantService = require('./consultant.service');
+module.exports.authService = require("./auth.service");
+module.exports.tokenService = require("./token.service");
+module.exports.userService = require("./user.service");
+module.exports.reservationService = require("./reservation.service");
+module.exports.consultantService = require("./consultant.service");
+module.exports.journalService = require("./journal.service");
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,4 +1,5 @@
-module.exports.authValidation = require('./auth.validation');
-module.exports.userValidation = require('./user.validation');
-module.exports.reservationValidation = require('./reservation.validation');
-module.exports.consultantValidation = require('./consultant.validation');
+module.exports.authValidation = require("./auth.validation");
+module.exports.userValidation = require("./user.validation");
+module.exports.reservationValidation = require("./reservation.validation");
+module.exports.consultantValidation = require("./consultant.validation");
+module.exports.journalValidation = require("./journal.validation");
